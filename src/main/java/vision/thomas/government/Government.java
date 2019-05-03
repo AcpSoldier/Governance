bfd206eb2544cbe2ef8094ce31884408ef4a3552
Thomas K <unconfigured@null.spigotmc.org>
2019-05-03 17:04:45 -0400

    Base project files.
    Just in case I ever need to revert back to an empty project, which I shouldn't need to do... Humble beginnings! :smile: I didn't know emojis worked in Git... :anguished:

---
+++ src/main/java/vision/thomas/government/Government.java
@@ -0,0 +1,17 @@
+package vision.thomas.government;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Government extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Shutdown logic
+    }
+}
