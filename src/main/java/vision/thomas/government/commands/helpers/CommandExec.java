335d419984b128227b856cecd59ee54596f7791b
Thomas K <unconfigured@null.spigotmc.org>
2019-05-03 21:16:14 -0400

    Sub command API
    API created by Max Lee (https://github.com/Phoenix616/). This will save me a lot of time. :smiley:

---
+++ src/main/java/vision/thomas/government/commands/helpers/CommandExec.java
@@ -0,0 +1,118 @@
+package vision.thomas.government.commands.helpers;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import vision.thomas.government.Government;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Copyright 2016 Max Lee (https://github.com/Phoenix616/)
+ * <p/>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Mozilla Public License as published by
+ * the Mozilla Foundation, version 2.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Mozilla Public License v2.0 for more details.
+ * <p/>
+ * You should have received a copy of the Mozilla Public License v2.0
+ * along with this program. If not, see <http://mozilla.org/MPL/2.0/>.
+ */
+
+public class CommandExec implements CommandExecutor {
+
+    private final Government plugin;
+
+    private Map<String, Map<String, SubCommand>> subCommands = new HashMap<String, Map<String, SubCommand>>();
+    private String header;
+
+    public CommandExec(Government plugin) {
+        this.plugin = plugin;
+        header = ChatColor.WHITE + plugin.getDescription().getAuthors().get(0) + "'s " +
+                ChatColor.GOLD + plugin.getName() +
+                ChatColor.WHITE + " v" + plugin.getDescription().getVersion();
+        plugin.getCommand(plugin.getName().toLowerCase()).setExecutor(this);
+    }
+
+    public void register(SubCommand sub) {
+        if (!subCommands.containsKey(sub.getCommand())) {
+            subCommands.put(sub.getCommand(), new LinkedHashMap<String, SubCommand>());
+        }
+        if (subCommands.get(sub.getCommand()).containsKey(sub.getPath())) {
+            throw new IllegalArgumentException("A sub command with the path '" + sub.getPath() + "' is already defined for command '" + sub.getCommand() + "'!");
+        }
+        subCommands.get(sub.getCommand()).put(sub.getPath(), sub);
+        try {
+            plugin.getServer().getPluginManager().addPermission(sub.getPermission());
+        } catch (IllegalArgumentException ignore) {
+            // Permission was already defined correctly in the plugin.yml
+        }
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (args.length == 0) {
+            List<String> helpText = new ArrayList<String>();
+            helpText.add(header);
+            if (subCommands.containsKey(cmd.getName())) {
+                for (SubCommand sub : subCommands.get(cmd.getName()).values()) {
+                    if (!sender.hasPermission(sub.getPermission())) {
+                        continue;
+                    }
+                    helpText.add(ChatColor.GOLD + sub.getUsage(label));
+                    helpText.add(ChatColor.WHITE + " " + sub.getHelp());
+                }
+            } else {
+                helpText.add("No sub commands found.");
+            }
+            sender.sendMessage(helpText.toArray(new String[helpText.size()]));
+            return true;
+        }
+
+        SubCommand sub = null;
+
+        int pathPartCount = 1;
+        if (subCommands.containsKey(cmd.getName())) {
+            String path = args[0];
+            while (!subCommands.get(cmd.getName()).containsKey(path) && pathPartCount < args.length) {
+                path += " " + args[pathPartCount].toLowerCase();
+                pathPartCount++;
+            }
+            sub = subCommands.get(cmd.getName()).get(path);
+        }
+
+        if (sub == null) {
+            if (subCommands.containsKey(cmd.getName())) {
+                Set<String> subCmdsStr = subCommands.get(cmd.getName()).keySet();
+                sender.sendMessage("Usage: /" + label + " " + Arrays.toString(subCmdsStr.toArray(new String[subCmdsStr.size()])));
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        if (!sender.hasPermission(sub.getPermission())) {
+            sender.sendMessage(plugin.getCommand(sub.getCommand()).getPermissionMessage().replace("<permission>", sub.getPermission().getName()));
+            return true;
+        }
+
+        String[] subArgs = new String[]{};
+        if (args.length > pathPartCount) {
+            subArgs = Arrays.copyOfRange(args, pathPartCount, args.length);
+        }
+        if (!sub.execute(sender, subArgs)) {
+            sender.sendMessage("Usage: " + sub.getUsage(label));
+        }
+        return true;
+    }
+}
---
+++ src/main/java/vision/thomas/government/commands/helpers/SubCommand.java
@@ -0,0 +1,94 @@
+package vision.thomas.government.commands.helpers;
+
+import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
+import vision.thomas.government.Government;
+
+/**
+ * Copyright 2016 Max Lee (https://github.com/Phoenix616/)
+ * <p/>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Mozilla Public License as published by
+ * the Mozilla Foundation, version 2.
+ * <p/>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Mozilla Public License v2.0 for more details.
+ * <p/>
+ * You should have received a copy of the Mozilla Public License v2.0
+ * along with this program. If not, see <http://mozilla.org/MPL/2.0/>.
+ */
+
+public abstract class SubCommand {
+
+    private final Government plugin;
+    private final String command;
+    private final String path;
+    private final Permission permission;
+    private final String arguments;
+    private final String help;
+
+    public SubCommand(Government plugin, String command, String path, String arguments, String help) {
+        this.plugin = plugin;
+        this.command = command;
+        this.path = path;
+        this.permission = new Permission(
+                plugin.getName().toLowerCase() + ".command." + getPath().replace(' ', '.').toLowerCase(),
+                "Get access to the /" + getCommand() + " " + getPath() + " subcommand.",
+                PermissionDefault.OP
+        );
+        this.arguments = arguments;
+        this.help = help;
+    }
+
+    public abstract boolean execute(CommandSender sender, String[] args);
+
+    public String getCommand() {
+        return command;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public Permission getPermission() {
+        return permission;
+    }
+
+    /**
+     * Get the help text
+     *
+     * @return The help text
+     */
+
+    public String getHelp() {
+        return help;
+    }
+
+    public Government getPlugin() {
+        return plugin;
+    }
+
+    /**
+     * Get the usage of this command containing command, path and arguments
+     *
+     * @return The usage in the pattern of "/command sub path [arguments...]"
+     */
+
+    public String getUsage() {
+        return getUsage(getCommand());
+    }
+
+    /**
+     * Get the usage of this command containing command, path and arguments
+     *
+     * @param label The name that should be displayed as the command
+     * @return The usage in the pattern of "/label sub path [arguments...]"
+     */
+
+    public String getUsage(String label) {
+        return "/" + label + " " + getPath() + " " + arguments;
+    }
+}
